687bb88d2233d5610f1f0b77b8394b13b0059cae
dshik <dshik@kr.ibm.com>
2016-12-08 13:55:12 +0900

    first commit

---
+++ src/main/java/com/ibm/klab/demo/SpringBootDemoAppApplication.java
@@ -0,0 +1,25 @@
+package com.ibm.klab.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoAppApplication.class, args);
+		
+//		ApplicationContext ctx  = SpringApplication.run(SpringBootDemoAppApplication.class, args);
+//		
+//		System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
+	}
+}
---
+++ src/main/java/com/ibm/klab/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.ibm.klab.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		logger.info("Welcome home REST Service !");
+		
+		return "<H1>Spring Boot REST Service [/]</H1>";
+	}
+}
